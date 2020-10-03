7a8d7fafa02495bed203b08b9eb863d0777e1268
Yang Hu <yuexiachanming@gmail.com>
2020-10-03 12:00:22 -0500

    Update wildcardMatching.go

--- wildcardMatching.go
+++ wildcardMatching.go
@@ -18,5 +18,30 @@ func isMatch(s string, p string) bool {
     // dp[i][j] = dp[i-1][j-1] if p[j] == ? || a-z, or 
     //            dp[i-1][j] => * matches s[i] || dp[i][j-1] => * is empty, if p[j] = *
     
+    // initialize a len(x) + 1 by len(y) + 1 2D slice
+    dp := make([][]bool, len(s) + 1)
+    for i := 0; i < len(dp); i++ {
+        dp[i] = make([]bool, len(p) + 1)
+    }
+    
+    // set empty cases 
+    dp[0][0] = true
+    
+    for i := 0; i < len(p); i++ {
+        if p[i] == '*' {
+            dp[0][i+1] = dp[0][i]
+        } 
+    }
+    
+    for i := 1; i < len(s) + 1; i++ {
+        for j := 1; j < len(p) + 1; j++ {
+            if p[j-1] == '*' {
+                dp[i][j] = dp[i-1][j] || dp[i][j-1]
+            } else if s[i-1] == p[j-1] || p[j-1] == '?' {
+                dp[i][j] = dp[i-1][j-1] 
+            }
+        }
+    }
+    return dp[len(s)][len(p)]
     
 }
