b83d060f3d39d7f12684683ee4b429e49c16956a
Yang Hu <yuexiachanming@gmail.com>
2020-10-13 09:16:07 -0500

    Update rotateImage.go

--- rotateImage.go
+++ rotateImage.go
@@ -1,18 +1,18 @@
 func rotate(matrix [][]int)  {
     // m'(i,j) = m(n-1-j, i)
-    // layer by layer
     n := len(matrix)
     for i := 0; i < (n+1) / 2; i ++ {
         for j := i; j < n-1-i; j ++ {
-            x,y, temp := i, j, matrix[i][j]
-            // rotate four numbers
+            x, y, temp := i, j, matrix[i][j]
+            // four numbers is a group. start at i, j
+            // the last number should be temp
             for {
                 if n-1-y==i && x == j {
                     matrix[x][y] = temp
                     break
                 }
                 matrix[x][y] = matrix[n-1-y][x]
-                x,y = n-1-y , x
+                x, y = n-1-y, x
             }
             
         }
